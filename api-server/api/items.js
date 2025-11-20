de9de975ee3286f4bddaf5a723c2dfbaed70426e
Oleg Barylko <olegbarylko81@gmail.com>
2025-11-20 20:04:35 +0200

    fixes

--- api-server/api/items.js
+++ api-server/api/items.js
@@ -1,27 +1,32 @@
-const axios = require('axios');
+const axios = require("axios");
 
-// Ваш API Token і URL
-const apiToken = 'TVRrMU16YzBPREl3TVRwR0x6NXlTbnc1TUY5WFJHdzZkVGR2VVRkUUpFQlVKVkZhVGowd0lVaHVVbDhwU1RCU08yazIg';
-const apiUrl = 'https://app.pe-gate.com/api/v1/client-admins/deals';
+// Your API Token and URL
+const apiToken =
+  "TVrrMU16YzBPRE1jTVRrwR0x6NXK1bTnc1TUY5WFJHddzZkVGR2VRkKUpFQ1VKVkZhVGowd0lVaHVbDhwU1RCU08yzazIg";
+const apiUrl = "https://app.pe-gate.com/api/v1/client-admins/deals";
 
 module.exports = async (req, res) => {
-    console.log("olko test");
+  console.log("olko test");
   try {
     const response = await axios.get(apiUrl, {
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiToken}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiToken}`,
       },
     });
 
-    // Вивести отриману відповідь для перевірки
-    console.log(response.data);
+    // Log response data for debugging
+    console.log("Response Data: ", response.data);
 
     res.status(200).json(response.data);
   } catch (error) {
-     console.log("error olko");
-    console.error(error);  // Покажемо помилки для зручності
-    res.status(500).json({ error: 'Щось пішло не так' });
+    // Log more detailed error info
+    console.error(
+      "Error fetching API data: ",
+      error.response ? error.response.data : error.message
+    );
+
+    res.status(500).json({ error: "Щось пішло не так" });
   }
 };
