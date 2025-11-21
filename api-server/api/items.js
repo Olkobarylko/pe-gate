49ea6843a3e1dd95d83ff3f18d77f568045195f7
Olkobarylko <olegbarylko81@gmail.com>
2025-11-21 22:47:28 +0200

    fixes

--- api-server/api/items.js
+++ api-server/api/items.js
@@ -10,7 +10,7 @@ const WEBFLOW_API_TOKEN = "27a1da0aeecafa64480b31bd281d1ba1224ad1095e9418d814456
 const PE_GATE_API_TOKEN = "MTk1Mzc0ODIwMTpTfHxYZH1wP3BiIUg1dChTa1B2JHxrUXJ1bUc5TlQ2VkZmYD5eWWMl";
 
 // Базовый URL Webflow v2 для CMS айтемов (staged items)
-const webflowApiUrl = `https://api.webflow.com/v2/collections/${WEBFLOW_COLLECTION_ID}/items`;
+const webflowApiUrl = `https://api.webflow.com/v2/collections/${WEBFLOW_COLLECTION_ID}/items/live`;
 
 // Утилита для slug
 function slugify(str) {
@@ -135,7 +135,8 @@ module.exports = async (req, res) => {
           dealtile3value: deal.dealTile3Value,
           dealoverviewcontent: deal.dealOverviewContent,
           "dealbackgroundimg-2": deal.dealBackgroundImg,
-
+          "isArchived": false,
+          "isDraft": false,
           // поле, по которому мы матчимся
           dealid: dealId,
         };
